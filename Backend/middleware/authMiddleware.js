bb82ab371abefabaa2de4014cfbcf9c06b8e8e81
greatkevingkpro-svg <greatkevingkpro@gmail.com>
2026-03-02 18:31:30 +0300

    Middleware test for my Backend

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken");
 // middleware that checks JWT
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  console.log("HEADERS:", req.headers);
-  console.log("AUTH HEADER:", req.headers.authorization);
+  // console.log("HEADERS:", req.headers);
+  // console.log("AUTH HEADER:", req.headers.authorization);
 
   if (!token) return res.status(401).json({ message: "No token provided" })
 
