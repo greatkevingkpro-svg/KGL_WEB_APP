14d8ce35a5344c7f4fe03073cfe93b0cfa805c7a
greatkevingkpro-svg <greatkevingkpro@gmail.com>
2026-03-06 10:56:54 +0300

    Remove Backend from branch Frontend

--- Backend/utils/customError.js
+++
@@ -1,9 +0,0 @@
-class KGLErrors extends Error{
-  constructor(message, statusCode, reason="") {
-    super(message);
-    this.statusCode = statusCode;
-    this.reason = reason;
-  }
-}
-
-module.exports = {KGLErrors}
