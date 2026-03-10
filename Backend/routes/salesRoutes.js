b7ae60551b65631ac839e1756847246b306fc361
greatkevingkpro-svg <greatkevingkpro@gmail.com>
2026-03-10 16:13:50 +0300

    Updating stock , sales, credit sales routes

--- Backend/routes/salesRoutes.js
+++ Backend/routes/salesRoutes.js
@@ -161,7 +161,7 @@ router.post("/", async (req, res, next) => {
 
     // Availability Check: Ensure store has enough produce
     const stock = await stockModel.findOne({
-      produceName: produceName.trim(),
+      produceName: { $regex: new RegExp(`^${produceName.trim()}$`, 'i') },
       branch: branch.trim()
     });
 
