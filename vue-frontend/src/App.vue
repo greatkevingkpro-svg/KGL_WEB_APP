71b6f89ae253814423e5b18ca27ed4394ebf768e
greatkevingkpro-svg <greatkevingkpro@gmail.com>
2026-03-24 22:15:42 +0300

    Add Netlify redirects

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 
 import "./assets/bootstrap/css/bootstrap.min.css"
-import "./assets/bootstrap/js/bootstrap.min.js"
+// import "./assets/bootstrap/js/bootstrap.min.js"
 import "vue3-toastify/dist/index.css";
 
 </script>
